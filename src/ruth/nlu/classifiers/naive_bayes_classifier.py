7541d219b54249142ee1fc886387d115ad0d2d49
prakashr7d <prakashr7d@gmail.com>
2022-02-20 20:22:48 +0530

    naive bayes train function implemented

--- src/ruth/nlu/classifiers/naive_bayes_classifier.py
+++ src/ruth/nlu/classifiers/naive_bayes_classifier.py
@@ -1,20 +1,25 @@
+import logging
 from typing import Any, Dict, List, Text
 
 import numpy as np
+import sklearn
 from ruth.nlu.classifiers.classifier import Classifier
 from ruth.shared.nlu.training_data.collections import TrainData
 from ruth.shared.nlu.training_data.ruth_data import RuthData
-from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelEncoder
+
+logger = logging.getLogger(__name__)
 
 
 class NaiveBayesClassifier(Classifier):
+    defaults = {"priors": None, "var_smoothing": 1e-9}
+
     def __init__(
         self,
         element_config: Dict[Text, Any],
         config: Dict[Text, Any] = None,
-        le: LabelEncoder = None,
-        clf: GridSearchCV = None,
+        le: "sklearn.model_selection.GridSearchCV" = None,
+        clf: "sklearn.preprocessing.LabelEncoder" = None,
     ):
         super(NaiveBayesClassifier, self).__init__(element_config=element_config)
 
@@ -25,15 +30,32 @@ class NaiveBayesClassifier(Classifier):
     def encode_the_str_to_int(self, labels: List[Text]) -> np.ndarray:
         return self.le.fit_transform(labels)
 
+    def _create_classifier(self) -> "sklearn.naive_bayes.GaussianNB":
+        from sklearn.naive_bayes import GaussianNB
+
+        priors = self.element_config["priors"]
+        var_smoothing = self.element_config["var_smoothing"]
+
+        return GaussianNB(priors=priors, var_smoothing=var_smoothing)
+
     def train(self, training_data: TrainData):
-        X = [
-            message.get_sparse_features() for message in training_data.training_examples
-        ]
+
+        intents = [message.intent for message in training_data.intent_examples]
+        if len(set(intents)) < 2:
+            logger.warning(
+                "There are no enough intent. "
+                "Atleast two unique intent are needed to train the model"
+            )
+            return
+
+        X = [message.get_sparse_features() for message in training_data.intent_examples]
         y = self.encode_the_str_to_int(
             [message.intent for message in training_data.training_examples]
         )
 
-        return X, y
+        X = np.reshape(X, (len(X), -1))
+        self.clf = self._create_classifier()
+        self.clf.fit(X, y)
 
     def parse(self, message: RuthData):
         pass
