f2694b0a1977518a3ebfc66c97eaa7e08032e485
vinithkac <65247102+vinithkac@users.noreply.github.com>
2022-03-12 14:35:13 +0530

    bert classifier without testing

---
+++ src/ruth/nlu/classifiers/transformers_finetuner.py
@@ -0,0 +1,33 @@
+import logging
+from typing import Dict, Text, Any
+
+from ruth.nlu.classifiers.classifier import Classifier
+from ruth.nlu.classifiers.constants import EPOCHS
+from ruth.shared.nlu.training_data.collections import TrainData
+
+logger = logging.getLogger(__name__)
+
+
+class TransformerClassifier:
+
+    @staticmethod
+    def get_default_config() -> Dict[Text, Any]:
+        return {
+        }
+
+    def preprocess_train_data(self):
+        pass
+
+    def __init__(self,
+                 config: Dict[Text, Any],
+                 ) -> None:
+        self.element_config = config
+
+    @classmethod
+    def create(cls,
+               config: Dict[Text, Any]):
+        return cls(config)
+
+    def train(self, training_data: TrainData):
+        model_data = self.preprocess_train_data(training_data)
+
