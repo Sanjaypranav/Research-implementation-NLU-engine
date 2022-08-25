ad5010721c85a73902b3021bc45bd0c4932f247a
prakashr7d <prakashr7d@gmail.com>
2022-08-25 10:48:42 +0530

    added the registry

--- src/ruth/nlu/registry.py
+++ src/ruth/nlu/registry.py
@@ -1,18 +1,21 @@
 from typing import Dict, Text
 
 from ruth.nlu.classifiers.naive_bayes_classifier import NaiveBayesClassifier
+from ruth.nlu.classifiers.svm_classifier import SVMClassifier
 from ruth.nlu.elements import Element
-from ruth.nlu.featurizers import HFFeaturizer
-from ruth.nlu.featurizers import (
-    CountVectorFeaturizer,
-)
+from ruth.nlu.featurizers.sparse_featurizers.count_vector_featurizer import CountVectorFeaturizer
+
+from ruth.nlu.tokenizer.whitespace_tokenizer import WhiteSpaceTokenizer
 
 element_classes = [
+    # Tokenizer
+    WhiteSpaceTokenizer,
     # Featurizers
     CountVectorFeaturizer,
     # Classifiers
     NaiveBayesClassifier,
-    HFFeaturizer,
+    SVMClassifier
+
 ]
 
 registered_classes: Dict[Text, Element] = {cls.name: cls for cls in element_classes}
--- tests/test_count_vectorizer.py
+++ tests/test_count_vectorizer.py
@@ -1,6 +1,8 @@
 import json
 from typing import Any, Dict, Text
 
+from nlu.registry import registered_classes
+
 from ruth.constants import PATH, TEXT
 from ruth.nlu.featurizers.sparse_featurizers.count_vector_featurizer import CountVectorFeaturizer
 
@@ -18,6 +20,24 @@ def test_count_vectorizer(count_featurizer_example: Dict[Text, Any]):
     training_data = TrainData(messages)
 
     featurizer = CountVectorFeaturizer({})
+    featurizer.train(training_data)
+    test_message = RuthData.build(text=count_featurizer_example[TEXT])
+    featurizer.parse(test_message)
+
+    assert (
+        count_featurizer_example[FEATURE]
+        == test_message.get_sparse_features().feature.toarray().tolist()
+    )
+
+def test_count_vectorizer(count_featurizer_example: Dict[Text, Any]):
+    messages = []
+    with open(count_featurizer_example[PATH], "r") as f:
+        example_data = json.load(f)
+    for data in example_data:
+        messages.append(RuthData(data=data))
+    training_data = TrainData(messages)
+
+    featurizer = registered_classes["CountVectorFeaturizer"].build({})
     featurizer.train(training_data)
     test_message = RuthData.build(text=count_featurizer_example[TEXT])
     featurizer.parse(test_message)
