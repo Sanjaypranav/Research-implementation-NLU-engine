d90b39db40c6164c7fcc40a14aaa7e8ea0fce015
Subash <subashs2018@gmail.com>
2022-08-26 20:44:34 +0800

    registry modified for import error of tfidf vectorizer

--- src/ruth/nlu/registry.py
+++ src/ruth/nlu/registry.py
@@ -4,7 +4,7 @@ from ruth.nlu.classifiers.hf_classifier import HFClassifier
 from ruth.nlu.classifiers.naive_bayes_classifier import NaiveBayesClassifier
 from ruth.nlu.classifiers.svm_classifier import SVMClassifier
 from ruth.nlu.elements import Element
-from ruth.nlu.featurizers.tfidf_featurizers.tfidf_vector_featurizer import TfidfVectorFeaturizer,
+from ruth.nlu.featurizers.tfidf_featurizers.tfidf_vector_featurizer import TfidfVectorFeaturizer
 
 from ruth.nlu.tokenizer.hf_tokenizer import HFTokenizer
 from ruth.nlu.tokenizer.whitespace_tokenizer import WhiteSpaceTokenizer
