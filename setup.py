56a1709485c44ea5629b37eea0c8ad31e1db0eab
Sanjaypranav <sanjaypriya195@gmail.com>
2022-09-25 17:09:01 +0530

    edited poetry

--- setup.py
+++ setup.py
@@ -11,17 +11,16 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 core_requirements = [
-    "click~=8.0.0",
-    "rich~=11.1.0",
+    "click~=8.0.1",
+    "rich~=12.0.0",
     "aiohttp~=3.6.3",
-    "pandas~=1.2.5",
     "jsonpickle~=2.1.0",
     "pyyaml~=6.0",
     "torch~=1.11.0",
     "regex",
     "scikit-learn~=1.0.2",
     "matplotlib~=3.6.0",
-    "progressbar~="
+    "progressbar~=2.5"
 ]
 
 
@@ -35,7 +34,7 @@ setuptools.setup(
     author_email='info@puretalk.ai',
     version=VERSION,
     install_requires=core_requirements,
-    python_requires='>=3.7,<3.9',
+    python_requires='>=3.7,<=3.10',
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
     include_package_data=True,
